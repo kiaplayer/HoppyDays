cb6f9ce75668d28dac7bee9b0e3b740256558928
kiaplayer <kiaplayer@gmial.com>
2020-10-02 23:12:13 +0300

    Refactoring

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -2,15 +2,18 @@ extends KinematicBody2D
 
 var motion = Vector2(0, 0)
 
-const SPEED = 1000
+const SPEED = 1500
 const GRAVITY = 300
 const UP = Vector2(0, -1)
 const JUMP_SPEED = 3000
+
+signal animate
 
 func _physics_process(delta):
 	apply_gravity()
 	jump()
 	move()
+	animate()
 	move_and_slide(motion, UP)
 
 func apply_gravity():
@@ -20,7 +23,7 @@ func apply_gravity():
 		motion.y += GRAVITY
 
 func jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
 
 func move():
@@ -30,3 +33,6 @@ func move():
 		motion.x = SPEED
 	else:
 		motion.x = 0	
+
+func animate():
+	emit_signal("animate", motion)

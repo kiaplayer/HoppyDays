12c2fb26e3d04ba45975aec07cbbc278335aba17
kiaplayer <kiaplayer@gmial.com>
2020-10-04 12:46:03 +0300

    Sound effects

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -32,6 +32,7 @@ func apply_gravity():
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
+		$JumpSFX.play()
 
 func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
@@ -52,5 +53,6 @@ func hurt():
 	yield(get_tree(), "idle_frame")
 	motion.y -= JUMP_SPEED
 	lives -= 1
+	$PainSFX.play()
 	if lives <= 0:
 		end_game()
